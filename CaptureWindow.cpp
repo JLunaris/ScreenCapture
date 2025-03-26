2bfbea337f94e83e66ec79080ac844956be0811e
jinzhaolong <jinzhaolong2020@163.com>
2025-03-26 17:35:37 +0800

    Init QRubberBand

--- CaptureWindow.cpp
+++ CaptureWindow.cpp
@@ -1,5 +1,8 @@
 #include "CaptureWindow.h"
 #include <QPainter>
+#include <QRubberBand>
+#include <QMouseEvent>
+#include <QPoint>
 
 void CaptureWindow::paintEvent(QPaintEvent *event)
 {
@@ -11,12 +14,32 @@ void CaptureWindow::paintEvent(QPaintEvent *event)
 
 void CaptureWindow::mousePressEvent(QMouseEvent *event)
 {
+    std::printf("mousePress\n");
+    QPoint pos {event->pos()};
+    if (rubberBandRect.isNull()) {
+        rubberBandRect.setTopLeft(pos);
+        rubberBand->setGeometry(rubberBandRect);
+        rubberBand->show();
+    }
+}
 
+void CaptureWindow::mouseMoveEvent(QMouseEvent *event)
+{
+    std::printf("mouseMove\n");
+    if(true){
+        QPoint pos {event->pos()};
+//        rubberBandRect->set
+    }
+
+}
+
+void CaptureWindow::mouseReleaseEvent(QMouseEvent *event)
+{
+    std::printf("mouseRelease\n");
 }
 
 CaptureWindow::CaptureWindow(QPixmap &&background, QWidget *parent)
         : background(std::move(background)), QWidget(parent, Qt::FramelessWindowHint)
 {
-
 
 }
--- CaptureWindow.h
+++ CaptureWindow.h
@@ -2,14 +2,20 @@
 
 #include <QWidget>
 #include <QPixmap>
+#include <QRubberBand>
+#include <QRect>
 
 class CaptureWindow : public QWidget
 {
     QPixmap background;
+    QRubberBand *rubberBand {new QRubberBand {QRubberBand::Rectangle, this}};
+    QRect rubberBandRect;
 
 protected:
     void paintEvent(QPaintEvent *event) override;
     void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
 
 public:
     explicit CaptureWindow(QPixmap &&background, QWidget *parent = nullptr);
