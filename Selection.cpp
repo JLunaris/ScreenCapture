834ccb62d9f8e523b49f29ae339a4bea7bf326a4
jinzhaolong <jinzhaolong2020@163.com>
2025-04-02 18:17:39 +0800

    Original Selection class

--- Selection.cpp
+++ Selection.cpp
@@ -1,11 +1,15 @@
 #include "Selection.h"
 #include <QPaintEvent>
 #include <QPainter>
+#include <QPen>
 
 void Selection::paintEvent(QPaintEvent *event)
 {
     QPainter painter {this};
-//    painter.fillRect(rect(), Qt::GlobalColor::yellow);
+    QPen pen {QColor {66, 133, 244}};
+    pen.setWidth(0); // 宽度设为0 可以避免 在移动选取时线条有时粗有时细
+    painter.setPen(pen);
+    painter.drawRect(rect().adjusted(1, 1, -1, -1));
 }
 
 Selection::Selection(QWidget *parent)
--- Selection.h
+++ Selection.h
@@ -1,15 +1,13 @@
 #pragma once
 
 #include <QWidget>
+#include <QRect>
 
 class Selection : public QWidget
 {
-
-
-
 protected:
     void paintEvent(QPaintEvent *event) override;
+
 public:
     explicit Selection(QWidget *parent = nullptr);
 };
-
