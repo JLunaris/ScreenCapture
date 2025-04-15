e2d9585588bf1b4918ba1dd3388df9a92f6c7d60
jinzhaolong <jinzhaolong2020@163.com>
2025-04-15 16:40:08 +0800

    完成工具面板的前端部分

--- UtilityPanel.cpp
+++ UtilityPanel.cpp
@@ -6,26 +6,39 @@
 UtilityPanel::UtilityPanel(CaptureWidget *parent)
         : QToolBar(parent)
 {
-//    setStyleSheet(R"(/* 按钮悬停/按下效果 */
-//QToolBar QToolButton:hover,
-//QToolBar QToolButton:pressed {
-//    background: #e0e0e0;
-//    border: 2px solid #a0a0a0;
-//    border-radius: 4px;
-//})");
-
     setStyleSheet(R"(
-QToolBar {
-    background-color: rgb(236, 240, 250);
-}
+        QToolBar {
+            background-color: rgb(236, 240, 250);
+        }
 
-QToolBar QToolButton:hover {
-    border: 1px solid rgb(164, 164, 164)
-}
-
+        QToolBar QToolButton:hover {
+            border: 1px solid rgb(164, 164, 164)
+        }
     )");
-    m_close->setIcon(QIcon{QPixmap{":/Close"}});
-    m_copy->setIcon(QIcon{QPixmap{":/Finish"}});
+
+    setIconSize(QSize {20, 24});
+
+    m_rectangle->setIcon(QIcon {QPixmap {":/Rectangle"}});
+    addWidget(m_rectangle);
+
+    m_ellipse->setIcon(QIcon {QPixmap {":/Ellipse"}});
+    addWidget(m_ellipse);
+
+    m_horizontalLine->setIcon(QIcon {QPixmap {":/HLine"}});
+    addWidget(m_horizontalLine);
+
+    m_pencil->setIcon(QIcon {QPixmap {":/Pencil"}});
+    addWidget(m_pencil);
+
+    m_redo->setIcon(QIcon {QPixmap {":/Redo"}});
+    addWidget(m_redo);
+
+    m_save->setIcon(QIcon {QPixmap {":/Save"}});
+    addWidget(m_save);
+
+    m_close->setIcon(QIcon {QPixmap {":/Close"}});
     addWidget(m_close);
+
+    m_copy->setIcon(QIcon {QPixmap {":/Finish"}});
     addWidget(m_copy);
 }
--- UtilityPanel.h
+++ UtilityPanel.h
@@ -8,6 +8,12 @@ class CaptureWidget; // forward declaration
 
 class UtilityPanel : public QToolBar
 {
+    QToolButton *m_rectangle {new QToolButton {this}};
+    QToolButton *m_ellipse {new QToolButton {this}};
+    QToolButton *m_horizontalLine {new QToolButton {this}};
+    QToolButton *m_pencil {new QToolButton {this}};
+    QToolButton *m_redo {new QToolButton {this}};
+    QToolButton *m_save {new QToolButton {this}};
     QToolButton *m_close {new QToolButton {this}};
     QToolButton *m_copy {new QToolButton {this}};
 
